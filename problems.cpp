95312658d95a6b67695dd9b7cfb6842891c14383
maryammalik5970 <maryammalik.5970@gmail.com>
2026-05-02 16:40:56 +0500

    add problems

--- problems.cpp
+++ problems.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";
+
+    // simple level problems
+    // // problem 1
+    // // cout<<"Hello World";
+    // cout<<"GeeksForGeeks"<<endl;
+
+
+    // // problem 2
+    // int a=4;
+    // float b=8;
+    // char c;
+    // cout<<a<<" "<<b<<" "<<c<<"1"<<endl;
+
+
+    // // problem 3
+    // int g=10;
+    // cout<<g<<endl;
+
+    
+    // // problem 4
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // double sum=a+b+c;
+    // cout<<"sum:"<<sum<<endl;
+
+
+
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ int a=3,b=4.2,c=23;
+    a+b+c;
+    cout<<a+b+c<<endl;
